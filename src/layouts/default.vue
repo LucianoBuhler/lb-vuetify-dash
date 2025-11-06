26ef82a87e5114b31bfabfaf5078f630ea33cb91
LucianoBuhler <lucianobuhler@gmail.com>
2025-11-06 16:32:17 -0300

    Init commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-layout class="rounded rounded-md border">
+    <v-app-bar 
+      title="Application bar"
+      absolute
+    ></v-app-bar>
+
+    <v-navigation-drawer absolute>
+      <v-list nav>
+        <v-list-item title="Navigation drawer" link></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex align-center justify-center" height="300">
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-layout>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
