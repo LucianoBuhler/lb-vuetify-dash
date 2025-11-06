44a10173fb0459ce28c31b72f4e93e5dde823200
LucianoBuhler <lucianobuhler@gmail.com>
2025-11-06 17:07:21 -0300

    Added user profile avatar + menu in the navbar

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,11 +1,27 @@
 <template>
   <v-layout class="rounded rounded-md border">
-    <v-app-bar 
-      title="Application bar"
-      absolute
-    ></v-app-bar>
+    <v-app-bar title="Application bar">
+      <template #append>
+         <v-menu>
+          <template v-slot:activator="{ props }">
+            <v-avatar v-bind="props" size="45" color="blue">
+              <span>LB</span>
+            </v-avatar>
+          </template>
+          <v-list>
+            <v-list-item
+              v-for="(item, index) in ['Profile', 'Settings', 'Logout']"
+              :key="index"
+              :value="index"
+            >
+              <v-list-item-title>{{ item }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </template>
+    </v-app-bar>
 
-    <v-navigation-drawer absolute>
+    <v-navigation-drawer>
       <v-list nav>
         <v-list-item title="Navigation drawer" link></v-list-item>
       </v-list>
