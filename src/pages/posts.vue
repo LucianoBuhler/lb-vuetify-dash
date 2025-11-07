8f16f6f2fbb94fd55f2ac2cab3cac246522624a0
LucianoBuhler <lucianobuhler@gmail.com>
2025-11-07 10:15:42 -0300

    Posts page: added data table component with filterin, pagination, sorting and seraching features

--- src/pages/posts.vue
+++ src/pages/posts.vue
@@ -1,33 +1,28 @@
 <template>
-  <div>
-    <h1>Posts</h1>
-    <v-table
-      :height="300"
-      :fixed-header="true"
-      density="comfortable"
-      :hover="true"
-    >
-      <thead>
-        <tr>
-          <th class="text-left">
-            Title
-          </th>
-          <th class="text-left">
-            Author
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="item in posts"
-          :key="item.title"
-        >
-          <td>{{ item.title }}</td>
-          <td>{{ item.author }}</td>
-        </tr>
-      </tbody>
-    </v-table>
-  </div>
+  <v-card
+    flat
+    title="Posts"
+  >
+    <template #text>
+      <v-text-field
+        v-model="search"
+        hide-details
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+        single-line
+        variant="outlined"
+      />
+    </template>
+
+    <v-data-table
+      v-model="selected"
+      :headers="headers"
+      item-value="title"
+      :items="posts"
+      :search="search"
+      show-select
+    />
+  </v-card>
 </template>
 
 <script setup lang="ts">
@@ -42,5 +37,22 @@
     { title: 'Post 6', author: 'Will' },
     { title: 'Post 7', author: 'Bill' },
     { title: 'Post 8', author: 'Zill' },
+  ])
+
+  const selected = ref([])
+  const search = ref([])
+
+  const headers = ref([
+    {
+      title: 'Post Title',
+      align: 'start',
+      sortable: true,
+      key: 'title',
+    },
+    {
+      title: 'Author',
+      align: 'end',
+      key: 'author'
+    },
   ])
 </script>
